0bcf519fbd5ed917c19e5a46236e59e6c15018e4
nerdoswmp <nerdoswmp>
2022-03-29 15:54:10 -0300

    Gets, sets e construtor Person.cs

--- Marketplace/Model/Person.cs
+++ Marketplace/Model/Person.cs
@@ -8,12 +8,81 @@ namespace Model
 {
     class Person
     {
+        // Vars
         protected string name;
         protected int age;
         protected string document;
         protected string email;
         protected string phone;
         protected string login;
+        protected Adress adress;
+
+        // Construtor
+        public Person(Adress adress)
+        {
+            this.adress = adress;
+        }
+
+        // gets | sets
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void setDoc(string document)
+        {
+            this.document = document;
+        }
+
+        public string getDoc()
+        {
+            return this.document;
+        }
+
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public string getEmail()
+        {
+            return this.email;
+        }
+
+        public void setPhone(string phone)
+        {
+            this.phone = phone;
+        }
+
+        public string getPhone()
+        {
+            return this.phone;
+        }
+
+        public void setLogin(string login)
+        {
+            this.login = login;
+        }
+
+        public string getLogin()
+        {
+            return this.login;
+        }
 
     }
 }
