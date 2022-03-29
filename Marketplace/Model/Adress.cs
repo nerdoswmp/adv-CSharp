b80265df65982cddc1b0b74a8e627393d8451cae
nerdoswmp <nerdoswmp>
2022-03-29 15:55:58 -0300

    Gets, sets e construtor Adress.cs

--- Marketplace/Model/Adress.cs
+++ Marketplace/Model/Adress.cs
@@ -8,10 +8,74 @@ namespace Model
 {
     internal class Adress
     {
+        // vars
         private string street;
         private string city;
         private string state;
         private string country;
         private string poste_code;
+
+        // Construtor
+        public Adress(string street, string city, string state, string country, string poste_code)
+        {
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.country = country;
+            this.poste_code = poste_code;
+        }
+
+        public Adress() { }
+
+        // gets | sets
+        public void setStreet(string street)
+        {
+            this.street = street;
+        }
+
+        public string getStreet()
+        {
+            return this.street;
+        }
+
+        public void setCity(string city)
+        {
+            this.city = city;
+        }
+
+        public string getCity()
+        {
+            return this.city;
+        }
+
+        public void setState(string state)
+        {
+            this.state = state;
+        }
+
+        public string getState()
+        {
+            return this.state;
+        }
+
+        public void setCountry(string country)
+        {
+            this.country = country;
+        }
+
+        public string getCountry()
+        {
+            return this.country;
+        }
+
+        public void setPostCode(string poste_code)
+        {
+            this.poste_code = poste_code;
+        }
+
+        public string getPostCode()
+        {
+            return this.poste_code;
+        }
     }
 }
