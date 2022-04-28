a3af8ebe2dda7f703b27c8abb3aeba1866d64f71
leosique <lokinhokkonopacki@gmail.com>
2022-04-28 16:49:12 -0300

    adicionado wishList

--- Marketplace/Controller/Controllers/WishListController.cs
+++ Marketplace/Controller/Controllers/WishListController.cs
@@ -1,21 +1,28 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using DTO;
-//namespace Controller.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class WishListController : ControllerBase
-//    {
-//        [HttpPost("adicionar")]
-//        public WishListDTO addProductToWishList([FromBody] Object request)
-//        {
-
-//            return null;
-//        }
-//        [HttpDelete("deletar")]
-//        public ProductDTO deleteProduct([FromBody] Object request)
-//        {
-//            return null;
-//        }
-//    }
-//}
+﻿using Microsoft.AspNetCore.Mvc;
+using DTO;
+namespace Controller.Controllers
+{
+    [ApiController]
+    [Route("wishList")]
+    public class WishListController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public object addProductToWishList([FromBody] WishListDTO wishList)
+        {
+            var wishListModel = Model.WishList.convertDTOToModel(wishList);
+            var id = wishListModel.save("85214789455", 1);
+            return new
+            {
+                id = id,
+                client = wishList.client,
+                produto = wishList.products
+            };
+        }                    
+        //[HttpDelete("deletar")]
+        //public ProductDTO deleteProduct([FromBody] Object request)
+        //{
+        //    return null;
+        //}
+    }
+}
