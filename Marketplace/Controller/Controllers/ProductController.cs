78189cb3cad08ac4a1d56ac3a365cc7414ee950b
leosique <lokinhokkonopacki@gmail.com>
2022-04-28 15:38:51 -0300

    push

---
+++ Marketplace/Controller/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DTO;
+
+namespace Controller.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("todos")]
+        public void allProducts()
+        {
+            Console.WriteLine("todos");          
+        }
+        [HttpPost("criar")]
+        public ProductDTO createProduct([FromBody] ProductDTO product)
+        {
+            return product;
+        }
+
+        [HttpDelete("deletar")]
+        public ProductDTO deleteProduct([FromBody] ProductDTO product)
+        {
+            return null;
+        }
+        [HttpPut("atualizar")]
+        public ProductDTO updateProduct([FromBody] ProductDTO product)
+        {
+            return null;
+        }
+    }
+}
---
+++ Marketplace/Controller/Controllers/WishListController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DTO;
+namespace Controller.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WishListController : ControllerBase
+    {
+        [HttpPost("adicionar")]
+        public WishListDTO addProductToWishList([FromBody] Object request)
+        {
+
+            return null;
+        }
+        [HttpDelete("deletar")]
+        public ProductDTO deleteProduct([FromBody] Object request)
+        {
+            return null;
+        }
+    }
+}
