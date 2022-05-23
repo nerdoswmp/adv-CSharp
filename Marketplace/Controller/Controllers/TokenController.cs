678e074fe7811d0b924e61ccea99af62c7f7eabf
nerdoswmp <nerdoswampo@gmail.com>
2022-05-23 14:50:58 -0300

    o churrasco vem

---
+++ Marketplace/Controller/Controllers/TokenController.cs
@@ -0,0 +1,61 @@
+namespace Controller.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using DTO;
+using Model;
+
+[ApiController]
+[Route("address")]
+public class TokenController : ControllerBase
+{
+    public IConfiguration _configuration;
+
+    public TokenController(IConfiguration config)
+    {
+        _configuration = config;
+    }
+
+    [HttpPost]
+    [Route("api")]
+    public IActionResult tokenGenerate([FromBody] ClientDTO login)
+    {
+        if (login != null && login.login != null && login.passwd != null)
+        {
+            var user = Model.Client.getLogin(login);
+            if (user != null)
+            {
+                var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("UserId", user.getId().ToString()),
+                    new Claim("UserName", user.getName()),
+                    new Claim("Email", user.getEmail())
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt: Key"]));
+                var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    _configuration["Jwt:Issuer"],
+                    _configuration["JwtAudience"],
+                    claims,
+                    expires: DateTime.UtcNow.AddMinutes(10),
+                    signingCredentials: signIn);
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            }
+            else
+            {
+                return BadRequest("Invalid credentials");
+            }
+        }
+        else
+        {
+            return BadRequest("Empty credentials");
+        }
+    }
+}
--- Marketplace/Controller/Program.cs
+++ Marketplace/Controller/Program.cs
@@ -1,15 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
@@ -19,8 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseAuthorization();
+app.UseAuthentication();
+//app.UseAuthorization();
 
 app.MapControllers();
 
